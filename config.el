aa1c2589b9969d274b1c13f0f018de60764b6555
Dom Parry <dom.parry@gmail.com>
2022-02-17 14:58:32 +0200

    first commit

---
+++ config.el
@@ -0,0 +1,167 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+
+(setq user-full-name "Dom Parry"
+      user-mail-address "dom.parry@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+
+;; Fira Code Setup
+
+(setq doom-font (font-spec :family "Fira Code" :size 13)
+      doom-variable-pitch-font (font-spec :family "Fira Code" :size 13)
+      doom-big-font (font-spec :family "Fira Code" :size 14))
+
+(let ((alist '((33 . ".\\(?:\\(?:==\\|!!\\)\\|[!=]\\)")
+               (35 . ".\\(?:###\\|##\\|_(\\|[#(?[_{]\\)")
+               (36 . ".\\(?:>\\)")
+               (37 . ".\\(?:\\(?:%%\\)\\|%\\)")
+               (38 . ".\\(?:\\(?:&&\\)\\|&\\)")
+               (42 . ".\\(?:\\(?:\\*\\*/\\)\\|\\(?:\\*[*/]\\)\\|[*/>]\\)")
+               (43 . ".\\(?:\\(?:\\+\\+\\)\\|[+>]\\)")
+               (45 . ".\\(?:\\(?:-[>-]\\|<<\\|>>\\)\\|[<>}~-]\\)")
+               (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+               ;; (46 . ".\\(?:\\(?:\\.[.<]\\)\\|[.=-]\\)")
+               (47 . ".\\(?:\\(?:\\*\\*\\|//\\|==\\)\\|[*/=>]\\)")
+               (48 . ".\\(?:x[a-zA-Z]\\)")
+               (58 . ".\\(?:::\\|[:=]\\)")
+               (59 . ".\\(?:;;\\|;\\)")
+               (60 . ".\\(?:\\(?:!--\\)\\|\\(?:~~\\|->\\|\\$>\\|\\*>\\|\\+>\\|--\\|<[<=-]\\|=[<=>]\\||>\\)\\|[*$+~/<=>|-]\\)")
+               (61 . ".\\(?:\\(?:/=\\|:=\\|<<\\|=[=>]\\|>>\\)\\|[<=>~]\\)")
+               (62 . ".\\(?:\\(?:=>\\|>[=>-]\\)\\|[=>-]\\)")
+               (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+               ;; (63 . ".\\(?:\\(\\?\\?\\)\\|[:=?]\\)")
+               (91 . ".\\(?:]\\)")
+               (92 . ".\\(?:\\(?:\\\\\\\\\\)\\|\\\\\\)")
+               (94 . ".\\(?:=\\)")
+               (119 . ".\\(?:ww\\)")
+               (123 . ".\\(?:-\\)")
+               (124 . ".\\(?:\\(?:|[=|]\\)\\|[=>|]\\)")
+               (126 . ".\\(?:~>\\|~~\\|[>=@~-]\\)"))))
+
+
+  (dolist (char-regexp alist)
+    (set-char-table-range composition-function-table (car char-regexp)
+                          `([,(cdr char-regexp) 0 font-shape-gstring]))))
+
+;; Maximize screen on start
+
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
+
+(setq-default evil-escape-key-sequence "fd")
+(setq! evil-want-Y-yank-to-eol nil)
+
+(when IS-MAC
+  (map! "s-x" #'kill-region))
+
+
+(doom-themes-neotree-config)
+(setq doom-themes-neotree-file-icons t)
+
+;; JS config
+
+(setq js-indent-level 2
+      js2-basic-offset 2)
+
+(setq +set-eslint-checker nil)
+
+(after! lsp-ui
+  ;; for whatever reason, this was running twice.
+  (setq lsp-ui-sideline-show-hover t)
+  (when (not +set-eslint-checker)
+    (progn
+      (setq +set-eslint-checker t)
+      (flycheck-add-mode 'javascript-eslint 'web-mode)
+      (flycheck-add-next-checker 'lsp-ui '(warning . javascript-eslint)))))
+
+(use-package! cider
+  :after clojure-mode
+  :config
+  (set-lookup-handlers! 'cider-mode nil))
+
+(use-package! clj-refactor
+  :after clojure-mode
+  :config
+  (set-lookup-handlers! 'clj-refactor-mode nil))
+
+(setq lsp-eldoc-enable-hover nil) ; disable lsp-mode showing eldoc during symbol at point
+(setq cljr-add-ns-to-blank-clj-files nil) ; disable clj-refactor adding ns to blank files
+
+(after! web-mode
+  (add-hook 'web-mode-hook #'flycheck-mode)
+
+  (setq web-mode-markup-indent-offset 2 ;; Indentation
+        web-mode-code-indent-offset 2
+        web-mode-enable-auto-quoting nil ;; disbale adding "" after an =
+        web-mode-auto-close-style 2))
+
+(after! web-mode
+  (remove-hook 'web-mode-hook #'+javascript-init-lsp-or-tide-maybe-h)
+  (add-hook 'web-mode-local-vars-hook #'+javascript-init-lsp-or-tide-maybe-h))
+
+(after! smartparens
+  (map! (:map smartparens-mode-map
+         "C->"       #'sp-backward-slurp-sexp
+         "C-."       #'sp-forward-slurp-sexp
+         "C-<"       #'sp-backward-barf-sexp
+         "C-,"       #'sp-forward-barf-sexp)))
+
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-material)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; Projecticle Setup
+
+(setq projectile-root-top-down-recurring t)
+
+(setq projectile-indexing-method 'alien
+      projectile-project-search-path '("~/Code/"))
+
+;; So Long only for very long lines
+(setq so-long-threshold 2000)
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+(setq lsp-ui-sideline-show-code-actions nil)
+(setq-default enable-local-variables t)
+
---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((cider-known-endpoints
+      ("tenandsix" "127.0.0.1" "11101")
+      ("panoramix" "127.0.0.1" "11102")
+      ("crismus-bonus" "127.0.0.1" "11103")
+      ("suffix" "127.0.0.1" "11104")
+      ("tenandsix-cljs" "127.0.0.1" "11001")
+      ("panoramix-cljs" "127.0.0.1" "11002")
+      ("suffix-cljs" "127.0.0.1" "11003"))
+     (eval cider-register-cljs-repl-type 'suffix-cljs "(do (require '[figwheel.main.api]) (figwheel.main.api/cljs-repl \"suffix\"))")
+     (eval cider-register-cljs-repl-type 'panoramix-cljs "(do (require '[figwheel.main.api]) (figwheel.main.api/cljs-repl \"panoramix\"))")
+     (eval cider-register-cljs-repl-type 'tenandsix-cljs "(do (require '[figwheel.main.api]) (figwheel.main.api/cljs-repl \"tenandsix\"))"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
